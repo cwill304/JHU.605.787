0f51721c9889d5ab3fa7235b1551a62bcac83afc
cwill304 <cwill304@jh.edu>
2022-03-12 18:34:55 -0600

    Finished core requirements

---
+++ module7-solution/js/app.js
@@ -0,0 +1,54 @@
+(function() {
+    "use strict";
+    angular.module("ShoppingListCheckOff", [])
+    .controller("ToBuyController", ToBuyController)
+    .controller("AlreadyBoughtController", AlreadyBoughtController)
+    .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ["ShoppingListCheckOffService"];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var itemsToBuy = this;
+        itemsToBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+        itemsToBuy.purchaseItem = function(index, item) {
+            ShoppingListCheckOffService.purchaseItem(index, item);
+        }   
+    }
+
+    AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var itemsAlreadyBought = this;
+        itemsAlreadyBought.items = ShoppingListCheckOffService.getItemsAlreadyBought();
+    }
+
+    function ShoppingListCheckOffService() {
+
+        var service = this;
+
+        var itemsToBuy = [
+            {name: "apples", quantity: "4", pricePerItem: 1.32},
+            {name: "bananas", quantity: "3", pricePerItem: 0.62},
+            {name: "cherries", quantity: "20", pricePerItem: 0.19},
+            {name: "oranges", quantity: "5", pricePerItem: 1.45},
+            {name: "pears", quantity: "2", pricePerItem: 1.08}
+        ];
+
+        var itemsAlreadyBought = [];
+        service.purchaseItem = function(index, item) {
+            var item = {
+                name: item.name,
+                quantity: item.quantity,
+                totalPricePaid: item.pricePerItem * item.quantity
+            }
+            itemsAlreadyBought.push(item);
+            itemsToBuy.splice(index, 1);
+        };
+
+        service.getItemsToBuy = function() {
+            return itemsToBuy;
+        }
+
+        service.getItemsAlreadyBought = function() {
+            return itemsAlreadyBought;
+        }
+    }
+})();
