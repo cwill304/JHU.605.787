775c52a786ad75f8e3aab37c9f59c03fc718c9c0
cwill304 <cwill304@jh.edu>
2022-03-26 00:40:42 -0500

    Updated to use directive

--- module8-solution/js/app.js
+++ module8-solution/js/app.js
@@ -3,7 +3,8 @@
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
-        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+        .directive('foundItems', FoundItemsDirective);
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
@@ -11,18 +12,21 @@
         menu.handleSearch = function () {
             var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
             promise.then(function (response) {
-                menu.items = response;
+                menu.found = response;
             })
             .catch(function (error) {
                 console.log(error);
             });
         }
+        menu.removeItem = function (itemIndex) {
+            console.log("remove yo");
+            menu.found.splice(itemIndex, 1);
+          };
     }
 
     MenuSearchService.$inject = ['$http', 'ApiBasePath'];
     function MenuSearchService($http, ApiBasePath) {
         var service = this;
-       
         service.getMatchedMenuItems = function (shortName) {
             var response = $http({
                 method: "GET",
@@ -30,6 +34,9 @@
                 searchTerm: shortName
             })
             .then(function (response) {
+                if (response.config.searchTerm == null || response.config.searchTerm == "") {
+                    return [];
+                }
                 var releventItems = (response.data.menu_items.filter(item => item.description.toLowerCase().includes(response.config.searchTerm.toLowerCase())));
                 return releventItems;
             })
@@ -39,4 +46,18 @@
             return response;
         };
     }
+
+    function FoundItemsDirective() {
+        var ddo = {
+          templateUrl: 'foundItem.html',
+          scope: {
+            found: '<',
+            onRemove: '&'
+          },
+          controller: NarrowItDownController,
+          controllerAs: 'menu',
+          bindToController: true
+        };
+        return ddo;
+      }
 })();
