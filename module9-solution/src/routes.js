b98df382302733165cbc794be3bb08d20c94ed8a
cwill304 <cwill304@jh.edu>
2022-04-26 22:20:23 -0500

    Finished 605.787 mod 9

--- module9-solution/src/routes.js
+++ module9-solution/src/routes.js
@@ -7,19 +7,14 @@ angular.module('MenuApp')
 RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 function RoutesConfig($stateProvider, $urlRouterProvider) {
 
-  // Redirect to home page if no other URL matches
   $urlRouterProvider.otherwise('/');
-
-  // *** Set up UI states ***
   $stateProvider
 
-  // Home page
   .state('home', {
     url: '/',
     templateUrl: 'src/categorylist/templates/home.template.html'
   })
 
-  // Categories page
   .state('categories', {
     url: '/categories',
     templateUrl: 'src/categorylist/templates/main-categorylist.template.html',
